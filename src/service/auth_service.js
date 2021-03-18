837137190c2109c43ec55fa9067be15ccfb4d3d8
grace179 <72989367+grace179@users.noreply.github.com>
2021-03-18 17:09:59 +0900

    add firebase

---
+++ src/service/auth_service.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+import firebaseApp from "./firebase";
+import "firebase/auth";
+
+class AuthService {
+  socialLogin(providerName){
+    const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
+    return firebaseApp.auth().signInWithPopup(authProvider);
+  }
+}
+
+export default AuthService;
