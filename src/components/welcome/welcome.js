f47eb8400f30f6347e8efe8a066bb79b09e1ec1e
grace179 <72989367+grace179@users.noreply.github.com>
2021-04-05 15:02:49 +0900

    update welcoe component

--- src/components/welcome/welcome.js
+++ src/components/welcome/welcome.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import styles from './welcome.module.css';
-import sampleImg from '../../common/images/default.jpg';
 
-const Welcome = (props) => {
+const Welcome = () => {
     return (
       <section className={styles.welcome}>
-        <img className={styles.image} src={sampleImg} alt="project"/>
+        <div className={styles.welcomeImg}>
+          <div className={styles.chracter}>
+            <div className={styles.eyes}>
+              <div>
+              <div className={styles.brow}></div>
+              <div className={styles.eye}></div>
+              </div>
+              <div>
+              <div className={styles.brow}></div>
+              <div className={styles.eye}></div>
+              </div>
+            </div>
+            <div className={styles.ears}>
+              <div className={styles.ear}></div>
+              <div className={styles.ear}></div>
+            </div>
+            <div className={styles.mouth}>
+              <div className={styles.nose}></div>
+              <div className={styles.elem}></div>
+              <div className={styles.elem}></div>
+            </div>
+          </div>
+        </div>
         <article className={styles.welcomeText}>방문해주셔서 감사합니다.<br/><br/>
         Portfolio<br/>
-        with React, Firebase
+        with React, Firebase<br/>
+        <h6>발전하기위해 노력하겠습니다.</h6>
         </article>
         <i className="fas fa-chevron-down"></i>
       </section>
